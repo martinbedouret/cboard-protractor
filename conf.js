e612a1f60383f485973274e61dfc6c1709214b28
martin bedouret <martinbedouret@gmail.com>
2018-07-12 14:08:32 -0300

    initial draft

---
+++ conf.js
@@ -0,0 +1,6 @@
+// conf.js
+exports.config = {
+  framework: 'jasmine',
+  seleniumAddress: 'https://cboard:5ab7f5b3-9ee3-4242-82c2-547cd8ffec04@ondemand.saucelabs.com/wd/hub',
+  specs: ['spec.js']
+}
