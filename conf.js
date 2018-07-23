f549e9bb13078e9a3efe11e1e65447d580f6bd3f
martin bedouret <martinbedouret@gmail.com>
2018-07-23 17:26:19 -0300

    wip

--- conf.js
+++ conf.js
@@ -2,5 +2,9 @@
 exports.config = {
   framework: 'jasmine',
   seleniumAddress: 'https://cboard:5ab7f5b3-9ee3-4242-82c2-547cd8ffec04@ondemand.saucelabs.com/wd/hub',
-  specs: ['spec.js']
+  specs: ['spec.js'],
+  multiCapabilities: [
+    {
+    browserName: 'microsoftedge'
+  }]
 }
--- spec.js
+++ spec.js
@@ -1,27 +1,22 @@
 // spec.js
-describe('Verify app main title', function() {
-  it('should have a title', function() {
+describe('Verify app main functionality', function() {
+  beforeEach(function() {
       browser.waitForAngularEnabled(false);
       browser.get('https://app.cboard.io/');
+  });
+  
+  it('should have a title', function() {
       expect(browser.getTitle()).toEqual('Cboard - AAC Communication Board');
   });
-});
 
-describe('Verify app login', function() {
   it('should login successfully', function() {
-      browser.waitForAngularEnabled(false);
-      browser.get('https://app.cboard.io/');
       expect(element(by.buttonText('Login')).click());
       expect(element(by.name('email')).sendKeys('martinbedouret@gmail.com'));
       expect(element(by.name('password')).sendKeys('Cireha32065'));
       expect(element(by.css('.jss56.jss32.jss41.jss42.jss44.jss45.jss138')).click());
       expect(element(by.css('.Navbar.Board__navbar')));
   });
-});
 
-describe('Verify app is operative', function() {
   it('should navigate thru pictograms', function() {
-      browser.waitForAngularEnabled(false);
-      browser.get('https://app.cboard.io/');
   });
 });
